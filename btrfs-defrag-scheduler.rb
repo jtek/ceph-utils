bdd9983d5419d0a89b75990b419f48fbad67e04e
Lionel Bouton <lionel.bouton@jtek.fr>
2017-03-16 11:54:33 +0100

    Less files processed by batch to minimize load impact

--- btrfs-defrag-scheduler.rb
+++ btrfs-defrag-scheduler.rb
@@ -143,7 +143,7 @@ MIN_DELAY_BETWEEN_FILEFRAGS = 5 / $speed_multiplier
 MAX_DELAY_BETWEEN_FILEFRAGS = 180
 # Batch size constraints for full refresh thread
 MAX_FILES_BATCH_SIZE = (250 * $speed_multiplier).to_i
-MIN_FILES_BATCH_SIZE = 50
+MIN_FILES_BATCH_SIZE = 10
 
 # We ignore files recently defragmented for 12 hours
 IGNORE_AFTER_DEFRAG_DELAY = 12 * 3600
