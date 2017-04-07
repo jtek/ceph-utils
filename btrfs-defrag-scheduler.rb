19232a365b887278e0e14e6c560955423b3aada3
Lionel Bouton <lionel.bouton@jtek.fr>
2017-04-08 01:40:59 +0200

    Allow faster defragmentation sequences and use a longer ignore delay

--- btrfs-defrag-scheduler.rb
+++ btrfs-defrag-scheduler.rb
@@ -147,10 +147,10 @@ MIN_DELAY_BETWEEN_FILEFRAGS = 2 / $speed_multiplier
 MAX_FILES_BATCH_SIZE = (500 * $speed_multiplier).to_i
 MIN_FILES_BATCH_SIZE = 10
 
-# We ignore files recently defragmented for 6 hours
-IGNORE_AFTER_DEFRAG_DELAY = 6 * 3600
+# We ignore files recently defragmented for 12 hours
+IGNORE_AFTER_DEFRAG_DELAY = 12 * 3600
 
-MIN_DELAY_BETWEEN_DEFRAGS = 0.1
+MIN_DELAY_BETWEEN_DEFRAGS = 0.02
 MAX_DELAY_BETWEEN_DEFRAGS = 10
 
 # How often do we dump a status update
