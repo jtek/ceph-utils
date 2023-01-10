c687c0bfcf64f90290370be67a0c104e01ed9a1b
Lionel Bouton <lionel.bouton@jtek.fr>
2023-01-10 14:18:50 +0100

    chore[btrfs-defrag-scheduler]: fix spelling error with no side effect

--- btrfs-defrag-scheduler.rb
+++ btrfs-defrag-scheduler.rb
@@ -2257,20 +2257,20 @@ class BtrfsDev
   def scan_status
     speed_factor = queue_speed_factor
     delay_speedup = @rate_controller.speedup_due_to_scan_delay
-    adjustement = ""
-    # No need to display speed adjustements when there aren't any
+    adjustment = ""
+    # No need to display speed adjustments when there aren't any
     if speed_factor <= 0.99 || delay_speedup >= 1.01
-      adjustement = " adj "
+      adjustment = " adj "
       msgs = []
       msgs << ("q&l:%.2f" % speed_factor) if speed_factor <= 0.99
       msgs << ("d:%.2f" % delay_speedup) if delay_speedup >= 1.01
-      adjustement << msgs.join(',')
+      adjustment << msgs.join(',')
     end
     "%d/%.3fs (%.3fs expected, IO %.0f%%) %s%s" %
       [ @rate_controller.current_batch_size,
         @rate_controller.current_batch_period, average_batch_time,
         @checker.load * 100, @rate_controller.relative_scan_speed_description,
-        adjustement ]
+        adjustment ]
   end
 
   def register_filefrag_speed(count:, time:, cached:)
